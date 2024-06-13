4d36e3515fb412a59ca7408c350460adb0510c27
Rodrigo Rojas <rodrigo.rjsmz@gmail.com>
2024-06-13 15:46:25 -0400

    part 1.7

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,10 +1,25 @@
 export function Statistics({ good, neutral, bad }) {
+  const averageScore = () => {
+    // good: 1, neutral: 0, bad: -1
+    const total = good + neutral + bad
+    const score = good - bad
+    return total === 0 ? 0 : score / total
+  }
+
+  const positivePercentage = () => {
+    const total = good + neutral + bad
+    const positive = good / total
+    return total === 0 ? '0%' : `${positive * 100}%`
+  }
   return (
     <div>
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {averageScore()}</p>
+      <p>positive {positivePercentage()}</p>
     </div>
   )
 }
