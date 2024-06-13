33708a8be732ac2f2a145be63bf9e74eee94dc9e
Rodrigo Rojas <rodrigo.rjsmz@gmail.com>
2024-06-13 19:01:28 -0400

    part 1.11

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,37 +1,41 @@
 const StatisticLine = ({ text, value }) => {
-  console.log('value', value)
   const { good, neutral, bad } = value
 
   const averageScore = () => {
     // good: 1, neutral: 0, bad: -1
     const total = good + neutral + bad
     const score = good - bad
-    return total === 0 ? 0 : score / total
+    return total === 0 ? 0 : Number.parseFloat(score / total).toFixed(1)
   }
 
   const positivePercentage = () => {
     const total = good + neutral + bad
     const positive = good / total
-    return total === 0 ? '0%' : `${positive * 100}%`
+    return total === 0
+      ? '0%'
+      : `${Number.parseFloat(positive).toFixed(3) * 100}%`
   }
 
   if (text === 'average')
     return (
-      <p>
-        {text} {averageScore()}
-      </p>
+      <tr>
+        <td>{text}</td>
+        <td>{averageScore()}</td>
+      </tr>
     )
 
   if (text === 'positive')
     return (
-      <p>
-        {text} {positivePercentage()}
-      </p>
+      <tr>
+        <td>{text}</td>
+        <td>{positivePercentage()}</td>
+      </tr>
     )
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -42,13 +46,18 @@ export function Statistics({ good, neutral, bad }) {
         <p>No feedback given</p>
       ) : (
         <>
-          <h1>Statistics</h1>
-          <StatisticLine text='good' value={good} />
-          <StatisticLine text='neutral' value={neutral} />
-          <StatisticLine text='bad' value={bad} />
-          <StatisticLine text='all' value={good + neutral + bad} />
-          <StatisticLine text='average' value={{ good, neutral, bad }} />
-          <StatisticLine text='positive' value={{ good, neutral, bad }} />
+          {' '}
+          <h2>Statistics</h2>
+          <table>
+            <tbody>
+              <StatisticLine text='good' value={good} />
+              <StatisticLine text='neutral' value={neutral} />
+              <StatisticLine text='bad' value={bad} />
+              <StatisticLine text='all' value={good + neutral + bad} />
+              <StatisticLine text='average' value={{ good, neutral, bad }} />
+              <StatisticLine text='positive' value={{ good, neutral, bad }} />
+            </tbody>
+          </table>
         </>
       )}
     </div>
