ab2b5757932f7441c9bfcc374584337a5e875b3a
Rodrigo Rojas <rodrigo.rjsmz@gmail.com>
2024-06-14 19:47:21 -0400

    part 2.2

--- part2/courseinfoP2/src/App.jsx
+++ part2/courseinfoP2/src/App.jsx
@@ -1,6 +1,6 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => <strong>Number of exercises {sum}</strong>
 
 const Part = ({ part }) => (
   <p>
