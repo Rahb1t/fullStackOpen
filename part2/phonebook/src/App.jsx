943b6bdb2a28ed7a6e74db184983699cc3d8fdd5
Rodrigo Rojas <rodrigo.rjsmz@gmail.com>
2024-06-14 20:47:32 -0400

    part 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,15 +4,20 @@ const Numbers = ({ persons }) => {
   return (
     <div>
       {persons.map((person) => (
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
       ))}
     </div>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '+56 9 1234 5678' },
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const validatePerson = (name) => {
     const existingPerson = persons.find((person) => person.name === name)
@@ -25,13 +30,19 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName }
+    const newPerson = { name: newName, number: newNumber }
     if (!validatePerson(newPerson.name)) return
     setPersons(persons.concat(newPerson))
+    setNewName('')
+    setNewNumber('')
   }
 
-  const handleChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -39,8 +50,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input onChange={handleChange} />
-          <div>debug: {newName}</div>
+          name: <input onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} />
         </div>
         <div>
           <button type='submit'>add</button>
